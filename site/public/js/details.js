59972956847b03e8652a7c319f475e7593c15b58
itggot-markus-moen <markus.moen@itggot.se>
2018-05-24 23:42:59 +0200

    Added an animation to the sellers site.

---
+++ site/public/js/details.js
@@ -0,0 +1,7 @@
+function toggleMenu(){
+    var element = document.querySelector(".details");
+    element.classList.toggle("show");
+
+    element = document.querySelector(".darken");
+    element.classList.toggle("fade")
+}
